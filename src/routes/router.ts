a9088255ee2fb660916c6cab79979c13fe3740aa
草梅友仁 <996881204@qq.com>
2020-05-27 17:08:28 +0800

    fix: 修复路由导入问题

---
+++ src/routes/router.ts
@@ -0,0 +1,10 @@
+import Router = require('koa-router')
+const router = new Router()
+
+import cmyr from './cmyr'
+router.use('/cmyr', cmyr.routes(), cmyr.allowedMethods())
+
+import pansou from './pansou'
+router.use('/pansou', pansou.routes(), pansou.allowedMethods())
+
+export default router
