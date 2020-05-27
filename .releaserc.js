dfbfb6ddc9faa5f2702784659df8772fcb0640f0
草梅友仁 <996881204@qq.com>
2020-05-27 17:21:33 +0800

    fix: 修复CI问题

--- .releaserc.js
+++ .releaserc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    verifyConditions: {
-        path: 'semantic-release-docker',
-        registryUrl: 'docker.io',
-    },
-    publish: {
-        path: 'semantic-release-docker',
-        name: 'caomeiyouren/super-search-hub',
-    },
+    // verifyConditions: {
+    //     path: 'semantic-release-docker',
+    //     registryUrl: 'docker.io',
+    // },
+    // publish: {
+    //     path: 'semantic-release-docker',
+    //     name: 'caomeiyouren/super-search-hub',
+    // },
     plugins: [
         [
             '@semantic-release/commit-analyzer', // 此处只导入解析规则 parserOpts
